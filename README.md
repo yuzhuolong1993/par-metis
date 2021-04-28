7e7c42d3b9bfa633980f5231dff67a086b89ce7f
yuzhuolong1993 <yuzhuolong1993@gmail.com>
2021-04-28 18:23:56 -0400

    tweak

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # par-metis
+
+## Install parmetis and openmpi
+Check [Install.txt](Install.txt) to install parmetis
+
+## Download and preprocess dataset
+- `python process_input.py papers`
+- `python process_input.py products`
+
+## Run parmetis
+`parmetis` takes six parameters: 
+- `parmetis <graph-file> <op-type> <nparts> <adapth-factor> <ipc2redist> <dbglvl> <seed>`
+
+For example, to partition `paper.graph` into 8 parts with single-process, multi-process, and multi-node:
+- Single-process: `parmetis paper.graph 1 8 1 1 6 1`
+
+- Multi-process: `mpirun -n 4 parmetis paper.graph 1 8 1 1 6 1`
+
+- Multi-node: `mpirun --host [ip_1],[ip_2],[ip_3],[ip_4] -np 4 parmetis paper.graph 1 8 1 1 6 1`
--- process_input.py
+++ process_input.py
@@ -1,38 +1,62 @@
+import sys
 import numpy as np
 from ogb.nodeproppred import NodePropPredDataset
 
-dataset = NodePropPredDataset(name = 'ogbn-papers100M', root='parm/')
-#dataset = NodePropPredDataset(name = 'ogbn-products', root='parm/')
-#dataset = NodePropPredDataset(name = 'ogbn-mag', root='parm/')
-graph, label = dataset[0]
+def print_usage():
+    print("python process_input.py papers")
+    print("python process_input.py products")
+    sys.exit()
+    
+def process(dataset_name):
+    if (dataset_name == "papers"):
+        dataset = NodePropPredDataset(name = 'ogbn-papers100M', root='parm/')
+        outf_name = "papers.graph"
+    elif (dataset_name == "products"):
+        dataset = NodePropPredDataset(name = 'ogbn-products', root='parm/')
+        outf_name = "products.graph"
+    else:
+        print_usage()
+        sys.exit(0)
 
-edge_index = graph['edge_index']
+    graph, label = dataset[0]
 
-lhs = edge_index[0]
-rhs = edge_index[1]
+    edge_index = graph['edge_index']
 
-number_nodes = max(max(lhs), max(rhs)) + 1
-#print(number_nodes)
+    lhs = edge_index[0]
+    rhs = edge_index[1]
 
-matrix = {}
-num_edges = 0
-for i in range(number_nodes):
-    matrix[i] = {i}
-    #matrix[i].append(i)
-    num_edges = num_edges + 1
+    number_nodes = max(max(lhs), max(rhs)) + 1
+    #print(number_nodes)
 
-for i in range(len(lhs)):
-    matrix[lhs[i]].add(rhs[i])
+    matrix = {}
+    num_edges = 0
+    for i in range(number_nodes):
+        matrix[i] = {i}
+        #matrix[i].append(i)
+        num_edges = num_edges + 1
 
-print(number_nodes, num_edges + len(lhs) // 2)
+    for i in range(len(lhs)):
+        matrix[lhs[i]].add(rhs[i])
 
-for x in matrix:
-    #matrix[x].sort()
-    #if matrix[x]==[]:
-    #    print(x+1, end=' ')
-    for y in matrix[x]:
-        if y=='':
-            print(y, end=' ')
-        else:
-            print(int(y)+1, end=' ')
-    print()
+    with open(outf_name, 'w') as fout:
+        sys.stdout = fout
+        print(number_nodes, num_edges + len(lhs) // 2)
+
+        for x in matrix:
+            #matrix[x].sort()
+            #if matrix[x]==[]:
+            #    print(x+1, end=' ')
+            for y in matrix[x]:
+                if y=='':
+                    print(y, end=' ')
+                else:
+                    print(int(y)+1, end=' ')
+            print()
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print_usage()
+        sys.exit(0)
+    process(sys.argv[1])
+
+    
